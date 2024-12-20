52074d0e33c5feadf5ea561727fa9afb016a8a51
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2024-12-20 17:23:54 +0100

    complete ex. dadi

--- dadi/script.js
+++ dadi/script.js
@@ -1,3 +1,16 @@
 /*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
 
-//genero un numero random
+//genero un numero random per l'utente
+const giocataUtente = Math.floor(Math.random() * 6) + 1; 
+
+//genero un numero random per il pc
+const giocataPc = Math.floor(Math.random() * 6) + 1; 
+
+// verifico chi vince
+
+if (giocataUtente > giocataPc) {
+    console.log("il numero", giocataUtente, "batte", giocataPc, "vince l'utente");
+}
+else {
+    console.log("il numero", giocataPc, "batte", giocataUtente, "vince il computer")
+}
