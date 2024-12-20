467c716acfea59bfc639b303c0cb70fcfa94b10d
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2024-12-20 16:08:02 +0100

    fix logic

--- email/script.js
+++ email/script.js
@@ -4,41 +4,36 @@ Non è consentito usare nessun metodo proprio degli array (come includes, per es
 Si può fare? Certo che si basta ragionare un po’.
 Nota: Non è necessario provvedere alla validazione delle email */
 
-
-//creo array con lista email
-//creo prompt per chiedere l'email all'utente
-//ciclo:
-    //creo condizionale per verificare che l'email inserita sia nella lista di chi può accedere
-    //stampo esito
-//fine ciclo
-
+//creo l'array con la lista delle email
 let listaInvitati = ["carlo.rossi@gmail.com", "luca.verdi@gmail.com", "maria.bianchi@gmail.com"];
-let emailInserite = [];
 
+//creo il prompt per chiedere all'utente di inserire la propria email
 let userEmail = prompt("inserisci la tua email");
 
-for (i = 0; i < 2; i++) {
+//ciclo 
+for (i = 0; i < 1; i++) {
+    //verifico se l'email inserita rientra nella lista di chi può accedere
     if (userEmail === listaInvitati[i]) {
+        //alert con esito positivo
         alert("Il tuo indirizzo e-mail è presente nella lista degli invitati: puoi partecipare alla festa");
-        emailInserite.push(userEmail);
-    }
-
-    else if (userEmail === emailInserite[i]) {
-        alert("Questo indirizzo e-mail è stato già registrato");
     }
         
     else {
+        //se l'utente sbaglia a digitare l'indirizzo, glielo chiedo di nuovo
         let userEmail = prompt("Controlla di aver digitato l'email correttamente. Hai solo un altro tentativo.")
+        //verifico se l'utente inserisce di nuovo un indirizzo sbagliato
         if (userEmail !== listaInvitati[i]) {
+        //alert con esito negativo
             alert("Il tuo indirizzo e-mail non è presente nella lista degli invitati: purtroppo non potrai partecipare alla festa.")
             
         }
+        //altrimenti alert con esito positivo
         else {
             alert("Il tuo indirizzo e-mail è presente nella lista degli invitati: puoi partecipare alla festa");
-        }
-    
+        }   
     }
 }
+//fine ciclo
 
 
 
