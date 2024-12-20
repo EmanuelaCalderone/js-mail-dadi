819ad90700b25ecc26935572974919f444db0220
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2024-12-20 13:41:31 +0100

    add logic first exercise

---
+++ script.js
@@ -0,0 +1,37 @@
+/* Mail
+Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota: Non è necessario provvedere alla validazione delle email */
+
+
+//creo array con lista email
+//ciclo:
+    //creo prompt per chiedere l'email all'utente
+    //creo condizionale per verificare che l'email inserita sia nella lista di chi può accedere
+    //stampo esito
+//fine ciclo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che “cosa” ci serve? */
